481eceb747c2506310f5f3b8f031deb9dd9a61a8
Hyrxs <hirxsmc@gmail.com>
2024-08-07 19:28:54 -0600

    Files

---
+++ BP/scripts/emoji_loader.js
@@ -0,0 +1,505 @@
+const _0x486976 = _0x2f7a;
+(function (_0x521e37, _0x1781d9) {
+    const _0x3be16a = _0x2f7a,
+        _0x4be1db = _0x521e37();
+    while (!![]) {
+        try {
+            const _0x4ec492 = -parseInt(_0x3be16a(341)) / 1 + -parseInt(_0x3be16a(322)) / 2 + -parseInt(_0x3be16a(362)) / 3 + -parseInt(_0x3be16a(348)) / 4 * (-parseInt(_0x3be16a(337)) / 5) + -parseInt(_0x3be16a(372)) / 6 * (parseInt(_0x3be16a(382)) / 7) + parseInt(_0x3be16a(324)) / 8 * (parseInt(_0x3be16a(334)) / 9) + -parseInt(_0x3be16a(344)) / 10 * (-parseInt(_0x3be16a(339)) / 11);
+            if (_0x4ec492 === _0x1781d9) break;
+            else _0x4be1db["push"](_0x4be1db["shift"]())
+        }
+        catch (_0x311026) {
+            _0x4be1db["push"](_0x4be1db["shift"]())
+        }
+    }
+})(_0x57bc, 754356);
+import {
+    system,
+    world
+}
+    from "@minecraft/server";
+
+function _0x57bc() {
+    const _0x2507c3 = ["90ZnIirp", "then", "Cube Emojis!", "190sINzZP", "body", "215875QvXNaZ", "emojis", "625426heXkIZ", "getText", "toString", "820YzJQWy", "hasTag", "addTag", "name", "116552vTlEVT", "beforeEvents", "gamerule sendcommandfeedback false", "Custom Emojis!", "0xE1", "getItem", "selection", "#custom emojis", "subscribe", "runCommand", "canceled", "Custom Emojis", "sender", "sourceEntity", "3344775AEQpiT", "translateEmojis", "#emojis", "Do you need a little help knowing the name of certain emojis?", "button", "forEach", "sendMessage", "slice", "Classic Emojis", "show", "24hmeawp", "accessibility.button.back", "Soon!", "runInterval", "getBlockFromViewDirection", "accessibility.button.close", "setText", "eic:book", "world", "eic", "1199450jCmHJO", "container", "system", "Heyy Welcome!\nEnjoy the add-on and remember to check out the emoji guide so you don't miss out on the personalized emojis I have for you!\nEnjoy~", "eic:firstJoin", "title", "fromCharCode", "minecraft:sign", "setItem", "textures/icons/classic_emojis", "scriptEventReceive", "textures/icons/custom_emojis", "Emoji-In-Chat | Welcome", "885534RZnqSG", "replaceAll", "725104eUxJUg", "textures/icons/like", "getComponent", "afterEvents", "selectedSlotIndex", "textures/icons/tnt", "keys", "list", "Emojis-In-Chat | Guide", "nameTag"];
+    _0x57bc = function () {
+        return _0x2507c3
+    };
+    return _0x57bc()
+}
+import * as _0x239b05 from "@minecraft/server";
+import * as _0x2001f6 from "@minecraft/server";
+import {
+    ActionFormData
+}
+    from "@minecraft/server-ui";
+const EIC = {
+    emojis:
+    {
+        "😀": "",
+        "😃": "",
+        "😄": "",
+        "😁": "",
+        "😆": "",
+        "🥹": "",
+        "😅": "",
+        "😂": "",
+        "🤣": "",
+        "🥲": "",
+        "☺️": "",
+        "😊": "",
+        "😇": "",
+        "🙂": "",
+        "🙃": "",
+        "😉": "",
+        "😌": "",
+        "😍": "",
+        "🥰": "",
+        "😘": "",
+        "😗": "",
+        "😙": "",
+        "😚": "",
+        "😋": "",
+        "😛": "",
+        "😝": "",
+        "😜": "",
+        "🤪": "",
+        "🤨": "",
+        "🧐": "",
+        "🤓": "",
+        "😎": "",
+        "🥸": "",
+        "🤩": "",
+        "🥳": "",
+        "😏": "",
+        "😒": "",
+        "😞": "",
+        "😔": "",
+        "😟": "",
+        "😕": "",
+        "🙁": "",
+        "☹️": "",
+        "😣": "",
+        "😖": "",
+        "😫": "",
+        "😩": "",
+        "🥺": "",
+        "😢": "",
+        "😭": "",
+        "😤": "",
+        "😠": "",
+        "😡": "",
+        "🤬": "",
+        "🤯": "",
+        "😳": "",
+        "🥵": "",
+        "🥶": "",
+        "😶‍🌫️": "",
+        "😱": "",
+        "😨": "",
+        "😰": "",
+        "😥": "",
+        "😓": "",
+        "🤗": "",
+        "🤔": "",
+        "🫣": "",
+        "🤭": "",
+        "🫢": "",
+        "🫡": "",
+        "🤫": "",
+        "🫠": "",
+        "🤥": "",
+        "😶": "",
+        "🫥": "",
+        "😐": "",
+        "🫤": "",
+        "😑": "",
+        "🫨": "",
+        "😬": "",
+        "🙄": "",
+        "😯": "",
+        "😦": "",
+        "😧": "",
+        "😮": "",
+        "😲": "",
+        "🥱": "",
+        "😴": "",
+        "🤤": "",
+        "😪": "",
+        ":face_exhaling:": "",
+        "😵": "",
+        ":face_with_spiral_eyes:": "",
+        "🤐": "",
+        "🥴": "",
+        "🤢": "",
+        "🤮": "",
+        "🤧": "",
+        "😷": "",
+        "🤒": "",
+        "🤕": "",
+        "🤑": "",
+        "🤠": "",
+        "😈": "",
+        "👿": "",
+        "👹": "",
+        "👺": "",
+        "🤡": "",
+        "💩": "",
+        "👻": "",
+        "💀": "",
+        "☠️": "",
+        "👽": "",
+        "👾": "",
+        "🤖": "",
+        "🎃": "",
+        "😺": "",
+        "😸": "",
+        "😹": "",
+        "😻": "",
+        "😼": "",
+        "😽": "",
+        "🙀": "",
+        "😿": "",
+        "😾": "",
+        "🤝": "",
+        "🫶": "",
+        "🤲": "",
+        "👐": "",
+        "🙌": "",
+        "👏": "",
+        "👍": "",
+        "👎": "",
+        "👊": "",
+        "✊": "",
+        "🤛": "",
+        "🤜": "",
+        "🫷": "",
+        "🫸": "",
+        "👋": "",
+        "🙏": "",
+        "💋": "",
+        "👄": "",
+        "🫦": "",
+        "🦷": "",
+        "👅": "",
+        "👁️": "",
+        "👀": "",
+        "🗿": "",
+        "🐱": "",
+        "🗣️": "",
+        "👈": "",
+        "👉": "",
+        "🔥": "",
+        "🍎": "",
+        "🍍": "",
+        ":pog:": "",
+        ":tilin:": "",
+        ":purple_guy:": "",
+        ":mewing:": "",
+        ":oreo:": "",
+        ":silly_face:": "",
+        ":aw_man:": "",
+        ":cringe:": "",
+        ":traumatized:": "",
+        ":grass:": "",
+        ":dirt:": "",
+        ":oak_log:": "",
+        ":oak_planks:": "",
+        ":stone:": "",
+        ":cobblestone:": "",
+        ":sand:": "",
+        ":gravel:": "",
+        ":bricks:": "",
+        ":netherrack:": "",
+        ":soul_sand:": "",
+        ":glow_stone:": "",
+        ":obsidian:": "",
+        ":crying_obsidian:": "",
+        ":glass:": "",
+        ":beacon:": "",
+        ":crafting_table:": "",
+        ":furnance:": "",
+        ":chest:": "",
+        ":ender_chest:": "",
+        ":redstone_lamp": "",
+        ":redstone_lamp_lit": "",
+        ":tnt:": "",
+        ":bedrock:": "",
+        ":command_block:": "",
+        ":missing_texture:": "",
+        ":grinning:": "",
+        ":smiley:": "",
+        ":smile:": "",
+        ":grin:": "",
+        ":laughing:": "",
+        ":face_holding_back_tears:": "",
+        ":sweat_smile:": "",
+        ":joy:": "",
+        ":rofl:": "",
+        ":smiling_face_with_tear:": "",
+        ":relaxed:": "",
+        ":blush:": "",
+        ":innocent:": "",
+        ":slight_smile:": "",
+        ":upside_down:": "",
+        ":wink:": "",
+        ":relieved:": "",
+        ":heart_eyes:": "",
+        ":smiling_face_with_3_hearts:": "",
+        ":kissing_heart:": "",
+        ":kissing:": "",
+        ":kissing_smiling_eyes:": "",
+        ":kissing_closed_eyes:": "",
+        ":yum:": "",
+        ":stuck_out_tongue:": "",
+        ":stuck_out_tongue_closed_eyes:": "",
+        ":stuck_out_tongue_winking_eye:": "",
+        ":zany_face:": "",
+        ":face_with_raised_eyebrow:": "",
+        ":face_with_monocle:": "",
+        ":nerd:": "",
+        ":sunglasses:": "",
+        ":disguised_face:": "",
+        ":star_struck:": "",
+        ":partying_face:": "",
+        ":smirk:": "",
+        ":unamused:": "",
+        ":disappointed:": "",
+        ":pensive:": "",
+        ":worried:": "",
+        ":confused:": "",
+        ":slight_frown:": "",
+        ":frowning2:": "",
+        ":persevere:": "",
+        ":confounded:": "",
+        ":tired_face:": "",
+        ":weary:": "",
+        ":pleading_face:": "",
+        ":cry:": "",
+        ":sob:": "",
+        ":triumph:": "",
+        ":angry:": "",
+        ":rage:": "",
+        ":face_with_symbols_over_mouth:": "",
+        ":exploding_head:": "",
+        ":flushed:": "",
+        ":hot_face:": "",
+        ":cold_face:": "",
+        ":face_in_clouds:": "",
+        ":scream:": "",
+        ":fearful:": "",
+        ":cold_sweat:": "",
+        ":disappointed_relieved:": "",
+        ":sweat:": "",
+        ":hugging:": "",
+        ":thinking:": "",
+        ":face_with_peeking_eye:": "",
+        ":face_with_hand_over_mouth:": "",
+        ":face_with_open_eyes_and_hand_over_mouth:": "",
+        ":saluting_face:": "",
+        ":shushing_face:": "",
+        ":melting_face:": "",
+        ":lying_face:": "",
+        ":no_mouth:": "",
+        ":dotted_line_face:": "",
+        ":neutral_face:": "",
+        ":face_with_diagonal_mouth:": "",
+        ":expressionless:": "",
+        ":shaking_face:": "",
+        ":grimacing:": "",
+        ":rolling_eyes:": "",
+        ":hushed:": "",
+        ":frowning:": "",
+        ":anguished:": "",
+        ":open_mouth:": "",
+        ":astonished:": "",
+        ":yawning_face:": "",
+        ":sleeping:": "",
+        ":drooling_face:": "",
+        ":sleepy:": "",
+        ":dizzy_face:": "",
+        ":zipper_mouth:": "",
+        ":woozy_face:": "",
+        ":nauseated_face:": "",
+        ":face_vomiting:": "",
+        ":sneezing_face:": "",
+        ":mask:": "",
+        ":thermometer_face:": "",
+        ":head_bandage:": "",
+        ":money_mouth:": "",
+        ":cowboy:": "",
+        ":smiling_imp:": "",
+        ":imp:": "",
+        ":japanese_ogre:": "",
+        ":japanese_goblin:": "",
+        ":clown:": "",
+        ":poop:": "",
+        ":ghost:": "",
+        ":skull:": "",
+        ":skull_crossbones:": "",
+        ":alien:": "",
+        ":space_invader:": "",
+        ":robot:": "",
+        ":jack_o_lantern:": "",
+        ":smiley_cat:": "",
+        ":smile_cat:": "",
+        ":joy_cat:": "",
+        ":heart_eyes_cat:": "",
+        ":smirk_cat:": "",
+        ":kissing_cat:": "",
+        ":scream_cat:": "",
+        ":crying_cat_face:": "",
+        ":pouting_cat:": "",
+        ":handshake:": "",
+        ":heart_hands:": "",
+        ":palms_up_together:": "",
+        ":open_hands:": "",
+        ":raised_hands:": "",
+        ":clap:": "",
+        ":thumbsup:": "",
+        ":thumbsdown:": "",
+        ":punch:": "",
+        ":fist:": "",
+        ":left_facing_fist:": "",
+        ":right_facing_fist:": "",
+        ":leftwards_pushing_hand:": "",
+        ":rightwards_pushing_hand:": "",
+        ":wave:": "",
+        ":pray:": "",
+        ":kiss:": "",
+        ":lips:": "",
+        ":biting_lip:": "",
+        ":tooth:": "",
+        ":tongue:": "",
+        ":eye:": "",
+        ":eyes:": "",
+        ":moyai:": "",
+        ":cat:": "",
+        ":speaking_head:": "",
+        ":point_left:": "",
+        ":point_right:": "",
+        ":fire:": "",
+        ":apple:": "",
+        ":pineapple:": ""
+    },
+    translateEmojis: function (_0x35140e) {
+        const _0x15a756 = _0x2f7a;
+        return Object[_0x15a756(330)](EIC[_0x15a756(340)])["forEach"](_0x2308a2 => {
+            const _0x574fcd = _0x15a756;
+            if (_0x2308a2 === "list") return;
+            _0x35140e = _0x35140e[_0x574fcd(323)]("" + _0x2308a2, "" + EIC[_0x574fcd(340)][_0x2308a2])
+        }), _0x35140e
+    }
+};
+for (let i = 14; i < 257; i++) {
+    EIC[_0x486976(340)]["c-" + i] = String[_0x486976(388)](parseInt(_0x486976(352) + Number(i)[_0x486976(343)](16)))
+}
+_0x239b05["world"][_0x486976(349)]["chatSend"][_0x486976(356)](_0x5792ed => {
+    const _0x5c7ee4 = _0x486976;
+    _0x5792ed["cancel"] = !![];
+    let _0x1c44fb = _0x5792ed["message"];
+    if (_0x1c44fb === _0x5c7ee4(364)) {
+        _0x5792ed[_0x5c7ee4(360)][_0x5c7ee4(368)](EIC[_0x5c7ee4(340)][_0x5c7ee4(331)](![]));
+        return
+    }
+    else {
+        if (_0x1c44fb === _0x5c7ee4(355)) {
+            _0x5792ed[_0x5c7ee4(360)]["sendMessage"](EIC[_0x5c7ee4(340)][_0x5c7ee4(331)](!![]));
+            return
+        }
+    }
+    _0x1c44fb = EIC[_0x5c7ee4(363)](_0x1c44fb), _0x239b05[_0x5c7ee4(384)]["run"](() => {
+        const _0x249ec7 = _0x5c7ee4;
+        _0x239b05[_0x249ec7(380)][_0x249ec7(368)]("<" + _0x5792ed[_0x249ec7(360)][_0x249ec7(347)] + "> " + _0x1c44fb)
+    })
+}), _0x239b05[_0x486976(384)][_0x486976(375)](() => {
+    const _0x47a6df = _0x486976;
+    world["getAllPlayers"]()[_0x47a6df(367)](_0x4f7ded => {
+        const _0x2a5725 = _0x47a6df;
+        if (_0x4f7ded) {
+            let _0x3cc354 = _0x4f7ded[_0x2a5725(326)]("minecraft:inventory")?.["container"]?.[_0x2a5725(353)](_0x4f7ded?.[_0x2a5725(328)]);
+            if (_0x3cc354) {
+                const _0x3539fa = _0x4f7ded["getComponent"]("minecraft:inventory")?.[_0x2a5725(383)],
+                    _0x4e563f = _0x3539fa[_0x2a5725(353)](_0x4f7ded?.[_0x2a5725(328)]);
+                let _0x4eec44 = _0x4e563f["nameTag"];
+                _0x4eec44 && _0x4eec44[_0x2a5725(369)](0, 2) !== "" && (_0x4e563f[_0x2a5725(333)] = "" + EIC[_0x2a5725(363)](_0x4eec44, EIC[_0x2a5725(340)][_0x2a5725(331)]), _0x3539fa[_0x2a5725(390)](_0x4f7ded?.[_0x2a5725(328)], _0x4e563f))
+            }
+            let _0x152cd8 = _0x4f7ded[_0x2a5725(376)]()?.["block"];
+            if (_0x152cd8) {
+                let _0x15e16a = _0x152cd8?.[_0x2a5725(326)](_0x2a5725(389))?.[_0x2a5725(342)]();
+                _0x15e16a && _0x152cd8?.[_0x2a5725(326)](_0x2a5725(389))?.[_0x2a5725(378)](EIC[_0x2a5725(363)](_0x15e16a))
+            }
+        }
+    })
+}), _0x2001f6["system"][_0x486976(327)][_0x486976(392)][_0x486976(356)](_0x19b4d4 => {
+    const _0x24b057 = _0x486976;
+    if (_0x19b4d4["id"] == _0x24b057(386) && !_0x19b4d4[_0x24b057(361)][_0x24b057(345)](_0x24b057(381))) firstJoin(_0x19b4d4[_0x24b057(361)]);
+    else _0x19b4d4["id"] == _0x24b057(379) && showBook(_0x19b4d4[_0x24b057(361)])
+});
+
+function _0x2f7a(_0x124162, _0x50e96e) {
+    const _0x57bce2 = _0x57bc();
+    return _0x2f7a = function (_0x2f7a08, _0x45c37f) {
+        _0x2f7a08 = _0x2f7a08 - 322;
+        let _0x2c036c = _0x57bce2[_0x2f7a08];
+        return _0x2c036c
+    }, _0x2f7a(_0x124162, _0x50e96e)
+}
+
+function firstJoin(_0x3dcf0c) {
+    const _0x1a852b = _0x486976,
+        _0x35e9e2 = (new ActionFormData)["title"](_0x1a852b(394))[_0x1a852b(338)](_0x1a852b(385))[_0x1a852b(366)](_0x1a852b(377), "textures/icons/like");
+    _0x3dcf0c["runCommand"](_0x1a852b(350)), _0x3dcf0c[_0x1a852b(357)]("give @s eic:emojipedia_book"), _0x3dcf0c[_0x1a852b(346)]("eic"), _0x35e9e2[_0x1a852b(371)](_0x3dcf0c)[_0x1a852b(335)](_0x5c9fbf => {
+        const _0x2adf8b = _0x1a852b;
+        if (_0x5c9fbf[_0x2adf8b(358)]) return;
+        switch (_0x5c9fbf[_0x2adf8b(354)]) {
+            case 0:
+                break
+        }
+    })
+}
+
+function showBook(_0x248367) {
+    const _0x113b2d = _0x486976,
+        _0x3fc8f2 = (new ActionFormData)[_0x113b2d(387)](_0x113b2d(332))[_0x113b2d(338)](_0x113b2d(365))[_0x113b2d(366)]("Classic Emojis!", _0x113b2d(391))["button"](_0x113b2d(351), _0x113b2d(393))[_0x113b2d(366)](_0x113b2d(336), _0x113b2d(329));
+    _0x3fc8f2["show"](_0x248367)[_0x113b2d(335)](_0x4b163a => {
+        const _0x27db3e = _0x113b2d;
+        if (_0x4b163a[_0x27db3e(358)]) return;
+        switch (_0x4b163a["selection"]) {
+            case 0:
+                viewClassicEmojis(_0x248367);
+                break;
+            case 1:
+                viewCustomEmojis(_0x248367);
+                break;
+            case 2:
+                viewCubeEmojis(_0x248367);
+                break
+        }
+    })
+}
+
+function viewClassicEmojis(_0x11f931) {
+    const _0x4a27b8 = _0x486976,
+        _0x20d3ed = (new ActionFormData)[_0x4a27b8(387)](_0x4a27b8(370))[_0x4a27b8(338)](_0x4a27b8(374))[_0x4a27b8(366)]("accessibility.button.back", _0x4a27b8(325));
+    _0x20d3ed[_0x4a27b8(371)](_0x11f931)[_0x4a27b8(335)](_0x1bc79f => {
+        const _0x146f24 = _0x4a27b8;
+        !_0x1bc79f["canceled"] && (_0x1bc79f[_0x146f24(354)] == 0 && showBook(_0x11f931))
+    })
+}
+
+function viewCustomEmojis(_0x1c922b) {
+    const _0x4716df = _0x486976,
+        _0x50bb2e = (new ActionFormData)[_0x4716df(387)](_0x4716df(359))[_0x4716df(338)](_0x4716df(374))[_0x4716df(366)](_0x4716df(373), _0x4716df(325));
+    _0x50bb2e["show"](_0x1c922b)[_0x4716df(335)](_0x3ac27d => {
+        const _0x48ddf1 = _0x4716df;
+        !_0x3ac27d[_0x48ddf1(358)] && (_0x3ac27d["selection"] == 0 && showBook(_0x1c922b))
+    })
+}
+
+function viewCubeEmojis(_0x85db88) {
+    const _0x35aab4 = _0x486976,
+        _0x30b491 = (new ActionFormData)["title"](_0x35aab4(336))["body"]("Soon!")[_0x35aab4(366)](_0x35aab4(373), _0x35aab4(325));
+    _0x30b491[_0x35aab4(371)](_0x85db88)[_0x35aab4(335)](_0x265522 => {
+        !_0x265522["canceled"] && (_0x265522["selection"] == 0 && showBook(_0x85db88))
+    })
+}
